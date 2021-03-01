48777f49539d162a62e0497439a58aef54e5b73a
Ant  Des <a.desfaits@orange.fr>
2021-03-01 09:15:22 +0100

    Completion

--- exercices/complexe/ComplexeEssai.java
+++ exercices/complexe/ComplexeEssai.java
@@ -1,10 +1,15 @@
 package GPI.exercices.complexe;
 
-import GPI.exercices.complexe.cartesien;
-import GPI.exercices.complexe.polaire;
+import GPI.exercices.complexe.cartesien.Complexe;
+import GPI.exercices.complexe.polaire.Complexe;
 
 public class ComplexeEssai{
+  static Complexe c1;
+  static Complexe c2;
   public static void main (String args[]){
-    
+    c1 = complexe.cartesien.Complexe();
+    c2 = complexe.polaire.Complexe();
+    c1.AfficheInfo();
+    c2.AfficheInfo();
   }
 }
